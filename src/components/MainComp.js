04d26c6d7d64fb212d766dc18c80b8c18a26a8b4
kwongihyeon <rrg1591rrg@naver.com>
2024-10-03 21:55:47 +0900

    ✨ feat : db서버 실행 키 설정, mainComp의 css수정 및 post-list에 db게시글제목 띄우기

--- src/components/MainComp.js
+++ src/components/MainComp.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../css/MainComp.css";
 
-const MainCompo = () => {
+const MainCompo = (props) => {
+  console.log(props.title);
   return (
-    <div>
-      <mainCompo />
+    <div className="line-change">
+      <span>
+        {props.title}
+        <br />
+      </span>
     </div>
   );
 };
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "../css/MainComp.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import MainCompo from "../components/MainComp";
 
 const Main = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/list").then((res) => {
+      setList(res.data);
+    });
+  }, []);
+
   return (
     <div className="container">
       <div className="board-tag">
         <div className="board-list"></div>
       </div>
-      <div className="post-list"></div>
+      <div className="post-list">
+        {list.map((item) => (
+          <MainCompo key={item.id} title={item.title} />
+        ))}
+      </div>
       <div className="user-info">
         <div className="user-login"></div>
         <div className="logout-btn"></div>
