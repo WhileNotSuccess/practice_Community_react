4e6821a7924819155da3a46af2069a61d4e58bd4
kwongihyeon <rrg1591rrg@naver.com>
2024-10-03 13:49:49 +0900

    🎨 style : mainPage css Nav에 들어가지 않는 css들 제작

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,26 +1,17 @@
 import React from "react";
-import "./Main.css";
+import "../css/MainComp.css";
 
 const Main = () => {
   return (
-    <div>
-      <div className="main-search"></div>
-      <div className="auto-layout"></div>
-      <div className="search-btn">
-        <img
-          src="http://localhost:3000/searchicon.png"
-          className="search-img"
-        ></img>
-      </div>
-      <div className="banner"></div>
+    <div className="container">
       <div className="board-tag">
         <div className="board-list"></div>
       </div>
+      <div className="post-list"></div>
       <div className="user-info">
         <div className="user-login"></div>
         <div className="logout-btn"></div>
       </div>
-      <div className="post-list"></div>
     </div>
   );
 };
