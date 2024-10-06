00b07b0ea6c21933c0cd7b9e86aa821a2c1f00f9
hyenseochoe <whahoo1@naver.com>
2024-10-06 14:09:02 +0900

    feat: 메인 페이지 상태 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,66 @@
 import React from "react";
-import MainCompo from "../components/MainComp";
+import "../css/MainComp.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { MainCompo, PostCompo } from "../components/MainComp";
+
 const Main = () => {
-  return <div>Main</div>;
+  const [list, setList] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/list").then((res) => {
+      setList(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/category").then((res) => {
+      setCategory(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const today = new Date();
+    setDate(
+      today
+        .toLocaleDateString("ko-kr", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        })
+        .replace(/\. /g, "/")
+        .replace(/\.$/, "")
+    );
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="board-tag">
+        <div className="board-list">
+          {category.map((category) => (
+            <PostCompo key={category.id} category={category} />
+          ))}
+        </div>
+      </div>
+      <div className="post-list">
+        {list.map((item) => (
+          <MainCompo
+            key={item.id}
+            title={item.title}
+            category={item.category}
+            date={date}
+          />
+        ))}
+      </div>
+      <div className="user-info">
+        <div className="user-login"></div>
+        <div className="logout-btn"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
